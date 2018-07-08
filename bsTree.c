785c22a2ec6dfa0fef190f260d27a2c829e3aec3
SteveWooo <461437874@qq.com>
2018-07-08 13:53:34 +0800

    avl树旋转完成

--- bsTree.c
+++ bsTree.c
@@ -18,18 +18,44 @@ BsNode* create_bsnode(int data){
     return p;
 }
 
-BsNode* rotateLeft(BsNode *p){
-    BsNode* temp;
-    temp = p->left;
+BsNode* leftRotate(BsNode* p){
+    BsNode* temp = p->left;
     p->left = temp->right;
     temp->right = p;
     
     p->heigh = getHeight(p->left) > getHeight(p->right) ?
         getHeight(p->left) + 1 : getHeight(p->right) + 1;
     
-    temp->heigh = getHeight(temp->left) > p->heigh ?
-        getHeight(temp->left) + 1 : p->heigh + 1;
+    temp->heigh = getHeight(temp->left) > getHeight(temp->right) ?
+        getHeight(temp->left) + 1 : getHeight(temp->right) + 1;
     return temp;
+}
+
+BsNode* rightRotate(BsNode* p){
+    BsNode* temp = p->right;
+    p->right = temp->left;
+    temp->left = p;
+    
+    p->heigh = getHeight(p->left) > getHeight(p->right) ?
+        getHeight(p->left) + 1 : getHeight(p->right) + 1;
+    temp->heigh = getHeight(p->left) > getHeight(p->right) ?
+        getHeight(p->left) + 1 : getHeight(p->right) + 1;
+    
+    return temp;
+}
+
+//10 3 4
+BsNode* doubleLeftRotate(BsNode* p){
+    p->left = rightRotate(p->left);
+    p = leftRotate(p);
+    return p;
+}
+
+//3 10 4
+BsNode* doubleRightRotate(BsNode* p){
+    p->right = leftRotate(p->right);
+    p = leftRotate(p);
+    return p;
 }
 
 int getHeight(BsNode* p){
@@ -45,23 +71,28 @@ BsNode* insert(BsNode* p, int data){
         p = create_bsnode(data);
         return p;
     }
-    if(data < p->data){
+    if(data <= p->data){
         p->left = insert(p->left, data);
-        //avl
-        if(getHeight(p->left)-getHeight(p->right) == 2){
-            if(data < p->left->data){
-                //左旋
-                p = rotateLeft(p);
+        //左边太高
+        if(getHeight(p->left) - getHeight(p->right) >= 2){
+            if(data > p->left->data){
+                p = doubleLeftRotate(p);
             } else {
-                
+                p = leftRotate(p);
             }
         }
-        
     } else {
         p->right = insert(p->right, data);
+        if(getHeight(p->right) - getHeight(p->left) >= 2){
+            if(data < p->right->data){
+                p = doubleRightRotate(p);
+            } else {
+                p = rightRotate(p);
+            }
+        }
     }
     p->heigh = getHeight(p->left) > getHeight(p->right) ?
-    getHeight(p->left) + 1 : getHeight(p->right) + 1;
+        getHeight(p->left) + 1 : getHeight(p->right) + 1;
     return p;
 }
 
@@ -76,33 +107,34 @@ BsNode* delete(BsNode* p, int data){
         return p;
     }
     
-    else{
+    else {
         
         if(p->left == NULL && p->right == NULL){
             p = NULL;
             return p;
         }
         
-        else if(p->left != NULL && p->right == NULL){
-            p = p->left;
-            return p;
-        }
-        
-        else if(p->left == NULL && p->right != NULL){
+        else if (p->left == NULL && p->right != NULL){
             p = p->right;
             return p;
         }
         
-        else{
+        else if (p->left != NULL && p->right == NULL){
+            p = p->left;
+            return p;
+        }
+        
+        else {
             BsNode* temp = p->right;
             while(temp->left != NULL){
                 temp = temp->left;
             }
             
             p->data = temp->data;
-            p->right = delete(p->right, temp->data);
+            p->right = delete(p->right, p->data);
             return p;
         }
+        
     }
 }
 
@@ -126,22 +158,81 @@ void read_lnr(BsNode* p){
 }
 
 void bsTree(){
-    BsNode* p = create_bsnode(5);
-//    p = insert(p, 9, 0);
-//    p = insert(p, 16, 0);
-//    p = insert(p, 13, 0);
-//    p = insert(p, 20, 0);
-//    p = insert(p, 12, 0);
-//    p = insert(p, 14, 0);
-//    p = insert(p, 18, 0);
-//    p = insert(p, 22, 0);
+    BsNode* p = create_bsnode(10);
+//    p = insert(p, 9);
+//    p = insert(p, 16);
+//    p = insert(p, 13);
+//    p = insert(p, 20);
+//    p = insert(p, 12);
+//    p = insert(p, 14);
+//    p = insert(p, 18);
+//    p = insert(p, 22);
 //    p = delete(p, 16);
     p = insert(p, 3);
-    p = insert(p, 2);
-//    p = insert(p, 4);
+    p = insert(p, 1);
+    p = insert(p, 4);
+    p = insert(p, 5);
 
     read_bstree(p);
     
-    printf("lnr:\n");
-    read_lnr(p);
+//    printf("lnr:\n");
+//    read_lnr(p);
 }
+
+/*
+ left rotate
+ BsNode* temp;
+ temp = p->left;
+ p->left = temp->right;
+ temp->right = p;
+ 
+ p->heigh = getHeight(p->left) > getHeight(p->right) ?
+ getHeight(p->left) + 1 : getHeight(p->right) + 1;
+ 
+ temp->heigh = getHeight(temp->left) > p->heigh ?
+ getHeight(temp->left) + 1 : p->heigh + 1;
+ return temp;
+ */
+
+/*
+ BsNode* delete(BsNode* p, int data){
+ if(data < p->data){
+ p->left = delete(p->left, data);
+ return p;
+ }
+ 
+ else if(data > p->data){
+ p->right = delete(p->right, data);
+ return p;
+ }
+ 
+ else{
+ 
+ if(p->left == NULL && p->right == NULL){
+ p = NULL;
+ return p;
+ }
+ 
+ else if(p->left != NULL && p->right == NULL){
+ p = p->left;
+ return p;
+ }
+ 
+ else if(p->left == NULL && p->right != NULL){
+ p = p->right;
+ return p;
+ }
+ 
+ else{
+ BsNode* temp = p->right;
+ while(temp->left != NULL){
+ temp = temp->left;
+ }
+ 
+ p->data = temp->data;
+ p->right = delete(p->right, temp->data);
+ return p;
+ }
+ }
+ }
+ */
